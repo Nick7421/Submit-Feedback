274b27a81842e091b235903da2a5587e0f690452
Nick Neiert <nickster7421@gmail.com>
2019-01-27 12:09:44 -0600

    added all needed components to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import { HashRouter as Router,Route, Link } from 'react-router-dom';
 import axios from 'axios';
+
+//Components needed for the routing pages
 import './App.css';
+import FeedbackFeeling from './../FeedbackFeeling/FeedbackFeeling.js';
+import FeedbackUnderstanding from './../FeedbackUnderstanding/FeedbackUnderstaning.js';
+import FeedbackSupport from './../FeedbackSupport/FeedbackSupport.js';
+import FeedbackComments from './../FeedbackComments/FeedbackComments.js';
+import FeedbackReview from './../FeedbackReview/FeedbackReview.js';
+
 
 class App extends Component {
   render() {
@@ -11,6 +20,7 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+
       </div>
     );
   }
