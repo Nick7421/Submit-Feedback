87b2b8b93b1de820120cf6b2b05d6a3c8ba1b30d
Nick Neiert <nickster7421@gmail.com>
2019-01-27 11:48:41 -0600

    created basic setup for componets for FeedbackComments, FeedBackSupport, and FeedbackUnderstanding.

---
+++ src/components/FeedbackComments/FeedbackComments.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class FeedbackComments extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Any comments you want to leave?</h1>
+      </div>
+    )
+  }
+}
+export default FeedbackComments;
---
+++ src/components/FeedbackSupport/FeedbackSupport.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class FeedbackSupport extends Component {
+  render() {
+    return (
+      <div>
+        <h1>How well are you bring supported?</h1>
+      </div>
+    )
+  }
+}
+export default FeedbackSupport;
