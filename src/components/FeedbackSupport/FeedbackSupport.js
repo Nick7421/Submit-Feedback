5a56c0802b5a7bee390551b949b51c665cef581b
Nick Neiert <nickster7421@gmail.com>
2019-01-27 17:40:01 -0600

    completed redux connection for component FeedbackSupport to send current state of input

--- src/components/FeedbackSupport/FeedbackSupport.js
+++ src/components/FeedbackSupport/FeedbackSupport.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react'
-import FeedbackReview from './../FeedbackReview/FeedbackReview'
+import React, { Component } from 'react';
+import FeedbackReview from './../FeedbackReview/FeedbackReview';
+import { connect } from 'react-redux';
 
 class FeedbackSupport extends Component {
-  render() {
+    contstructor(){
+        super();
+        this.state = {
+          understanding:'',
+      }
+
+    }
+    handleChange = (event) => {
+        this.setState({
+            understanding: event.target.value,
+        });
+    }
+  
+    handleNext = (event) => {
+        const action = { type: 'Submit_Understanding_Info', payload: this.state };
+        this.props.dispatch(action);
+        this.props.history.push('/feedback-support');
+        
+        }
+    render() {
+      
     return (
         <div>
       <div>
         <h1>How well are you bring supported?</h1>
+        <input onChange={this.handleChange}type ="Number" placeholder = "enter a number 1-5 " value = {this.state.support}></input>
+        <button onClick={this.handleNext}>Next</button>
       </div>
       <FeedbackReview/>
       </div>
     )
   }
 }
-export default FeedbackSupport;
+export default connect()(FeedbackSupport);
