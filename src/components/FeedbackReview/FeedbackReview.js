35dd44aca557c14c1c580efb5e767985f7f73ce2
Nick Neiert <nickster7421@gmail.com>
2019-01-27 11:55:26 -0600

    Completed basic setup for component FeedbackReview

---
+++ src/components/FeedbackReview/FeedbackReview.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class FeedbackReview extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Review Your Feedback</h1>
+        <p></p>
+        <p>Feelings:</p>
+        <p>Understanding:</p>
+        <p>Support:</p>
+        <p>Comments:</p>
+      </div>
+    )
+  }
+}
+export default FeedbackReview;
