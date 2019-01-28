3414926ae87dc9e1adbb775a15e9bb0cf9534bc3
Nick Neiert <nickster7421@gmail.com>
2019-01-28 16:01:15 -0600

    Added build folder for Keroku.

---
+++ build/precache-manifest.d36730eae1ad54ec2bf3663130b0bb05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92055ec2b1794596a227",
+    "url": "/static/js/main.92055ec2.chunk.js"
+  },
+  {
+    "revision": "afafb88fc73ca3a95cf3",
+    "url": "/static/js/1.afafb88f.chunk.js"
+  },
+  {
+    "revision": "92055ec2b1794596a227",
+    "url": "/static/css/main.a793c8b3.chunk.css"
+  },
+  {
+    "revision": "7e938e18d32fcbd377665904fcbce946",
+    "url": "/index.html"
+  }
+];
